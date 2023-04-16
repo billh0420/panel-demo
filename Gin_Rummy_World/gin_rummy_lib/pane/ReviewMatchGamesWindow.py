9ff8947fda377fba33e6b0e41464796b2fbb44a9
William Hale <billh04@yahoo.com>
2023-04-15 22:38:44 -0500

    Rework DQNAgent

--- Gin_Rummy_World/gin_rummy_lib/pane/ReviewMatchGamesWindow.py
+++ Gin_Rummy_World/gin_rummy_lib/pane/ReviewMatchGamesWindow.py
@@ -10,17 +10,22 @@ class ReviewMatchGamesWindow(pn.Tabs):
 
     def __init__(self, world: World):
         super().__init__()
-        self.world = world
-        self.append(('Review', ReviewPlayWindow(world=self.world)))
-        self.append(('DQNAgent', DQNAgentPane(world=self.world)))
-        self.append(('Trainer', self.trainer_view))
-        self.append(('GameSettings', GameSettingsPane(world=self.world)))
+        self.append(('Review', ReviewPlayWindow(world=world)))
+        self.append(('DQNAgent', self.dqn_agent_view(world=world)))
+        self.append(('Trainer', self.trainer_view(world=world)))
+        self.append(('GameSettings', GameSettingsPane(world=world)))
         self.dynamic = True
         self.height = 800
 
-    @property
-    def trainer_view(self):
+    def dqn_agent_view(self, world):
+        title = pn.pane.Markdown("# DQN Agent Settings")
+        dqn_agent_pane = DQNAgentPane(world=world)
+        dqn_agent_view = pn.Column(title, dqn_agent_pane)
+        dqn_agent_view.width_policy = 'max'
+        return dqn_agent_view
+
+    def trainer_view(self, world):
         trainer_title = pn.pane.Markdown("# RL Trainer Settings")
-        trainer_pane = TrainerPane(world=self.world)
+        trainer_pane = TrainerPane(world=world)
         trainer_view = pn.Column(trainer_title, trainer_pane, sizing_mode = 'stretch_width')
         return trainer_view
